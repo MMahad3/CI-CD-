667b9a6717a7e64ab29ca195aca9d8ac648bdac0
Mahad Munir <mahadmunir6@gmail.com>
2025-11-24 16:16:23 +0500

    Testing the pipeline

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ import { increment } from '../utils.js';
 
 const btn = document.getElementById('countBtn');
 const display = document.getElementById('count');
+const a = ;  // <-- Missing value after =
 
 let value = 0;
 btn.addEventListener('click', () => {
